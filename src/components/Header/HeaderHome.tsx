034a4f1d248f6b9f783ad1fbbfce96464b10a31f
facuzero <facuzero_95@hotmail.com>
2024-04-24 13:57:41 -0300

    cambio css

--- src/components/Header/HeaderHome.tsx
+++ src/components/Header/HeaderHome.tsx
@@ -1,9 +1,9 @@
-import './ExploreContainer.css';
+import '../../styles/general.css';
 import logOut from '../../images/log-out.webp';
 
 const ExploreContainer: React.FC = () => {
   return (
-    <header id='container'>
+    <header className='azulMorado'>
       <strong>LavaRiso</strong>
       <img src={logOut} alt='cerrar sesión' />
     </header>
