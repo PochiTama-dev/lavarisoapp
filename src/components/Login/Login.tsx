ce6805deccf61f39df385839df10837e1b39a1df
GonzaEnrique27 <gonz_1994@hotmail.com>
2024-05-28 17:31:42 -0300

    agregue ion-alert y corregi otro error

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -14,17 +14,29 @@ import { useHistory } from "react-router-dom";
 const LoginComponent: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
   const history = useHistory();
+
+  const handleEmailChange = (e: CustomEvent) => {
+    setEmail(e.detail.value!);
+  };
+
+  const handlePasswordChange = (e: CustomEvent) => {
+    setPassword(e.detail.value!);
+  };
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
     if (!email || !email.includes("@") || !email.includes(".com")) {
-      alert("Por favor, ingrese un correo electrónico válido.");
+      setAlertMessage("Por favor, ingrese un correo electrónico válido.");
+      setShowAlert(true);
       return;
     }
 
     if (!password) {
-      alert("Ingrese su contraseña");
+      setAlertMessage("Ingrese su contraseña");
+      setShowAlert(true);
       return;
     }
     history.push("/rol");
@@ -33,6 +45,12 @@ const LoginComponent: React.FC = () => {
   return (
     <IonPage className="login">
       <IonContent>
+        <IonAlert
+          isOpen={showAlert}
+          onDidDismiss={() => setShowAlert(false)}
+          message={alertMessage}
+          buttons={["Cerrar"]}
+        />
         <div className="login-content">
           <h1>LavaRiso</h1>
           <h2>Credenciales requeridas</h2>
@@ -42,14 +60,14 @@ const LoginComponent: React.FC = () => {
               type="text"
               placeholder="E-mail"
               value={email}
-              onIonChange={(e) => setEmail(e.detail.value!)}
+              onIonChange={handleEmailChange}
             />
             <IonInput
               className="inputs"
               type="password"
               placeholder="Contraseña"
               value={password}
-              onIonChange={(e) => setPassword(e.detail.value!)}
+              onIonChange={handlePasswordChange}
             />
             <IonButton className="login-button" type="submit">
               Iniciar sesión
