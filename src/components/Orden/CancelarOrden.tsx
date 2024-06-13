70eaf43f57f8d30b18fbe34299723d10dc49ea61
NachoMaraude <maraudenacho@gmail.com>
2024-06-12 22:40:44 -0300

    LVM-16 // primeros arreglos para cancelar ordenes

--- src/components/Orden/CancelarOrden.tsx
+++ src/components/Orden/CancelarOrden.tsx
@@ -1,31 +1,77 @@
-import { IonButton, IonContent } from '@ionic/react';
-import './ConfirmacionOrden.css';
-import ModalCancelacionComponent from './ModalCancel';
-import { useState } from 'react';
+import { IonButton, IonContent } from "@ionic/react";
+import "./ConfirmacionOrden.css";
+import ModalCancelacionComponent from "./ModalCancel";
+import { useState } from "react";
 
-function CancelacionOrdenComponent() {
+interface CancelacionOrdenComponentProps {
+  onCancel: () => void;
+  // onClose: () => void;
+}
+const CancelacionOrdenComponent: React.FC<CancelacionOrdenComponentProps> = ({
+  onCancel,
+}) => {
   const [modal, setModal] = useState(false);
+  const [detalles, setDetalles] = useState("");
 
-  const numeroOrden = '#25645';
+  const numeroOrden = 1;
 
   const handleModal = () => {
     setModal(!modal);
   };
+
+  const handleCancelOrder = async () => {
+    try {
+      const response = await fetch(
+        `https://lv-back.online/ordenes/modificar/${numeroOrden}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id_tipo_estado: 2, // 2 es el ID para el estado "cancelada"
+            // motivo: detalles,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        alert("Orden cancelada exitosamente");
+        setModal(false);
+        // Actualiza el estado local o redirige si es necesario
+      } else {
+        alert("Error al cancelar la orden");
+      }
+    } catch (error) {
+      console.error("Error al cancelar la orden:", error);
+      alert("Error al cancelar la orden");
+    }
+  };
   return (
-    <IonContent className='confirmacion-orden-container'>
-      <div className='confirmacion-orden-top-box'>
+    <IonContent className="confirmacion-orden-container">
+      <div className="confirmacion-orden-top-box">
         <h1>
           <strong>Cancelar Orden</strong>
         </h1>
         <h3>La orden de trabajo No.{numeroOrden} está por cancelarse</h3>
       </div>
-      <div className='confirmacion-orden-medium-box'>
-        <textarea name='' id='' placeholder='Detalles del incumplimiento' rows={10}></textarea>
-        <IonButton onClick={handleModal}>Cancelar orden</IonButton>
+      <div className="confirmacion-orden-medium-box">
+        <textarea
+          name=""
+          id=""
+          placeholder="Detalles del incumplimiento"
+          rows={10}
+        ></textarea>
+        <IonButton onClick={handleCancelOrder}>Cancelar orden</IonButton>
       </div>
-      {modal && <ModalCancelacionComponent />}
+      {modal && (
+        <ModalCancelacionComponent
+          onCancel={handleCancelOrder}
+          onClose={handleModal}
+        />
+      )}
     </IonContent>
   );
-}
+};
 
 export default CancelacionOrdenComponent;
--- src/pages/Entrega/Entrega.tsx
+++ src/pages/Entrega/Entrega.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   IonContent,
   IonPage,
@@ -15,36 +15,38 @@ import {
   IonRow,
   IonCol,
   IonHeader,
-} from '@ionic/react';
-import { pencilOutline } from 'ionicons/icons';
-import '../Diagnostico/diagnostico.css';
-import SignatureCanvas from 'react-signature-canvas';
-import './entrega.css';
-import CancelarOrden from '../../components/Orden/CancelarOrden';
-import HeaderGeneral from '../../components/Header/HeaderGeneral';
+} from "@ionic/react";
+import { pencilOutline } from "ionicons/icons";
+import "../Diagnostico/diagnostico.css";
+import SignatureCanvas from "react-signature-canvas";
+import "./entrega.css";
+import CancelarOrden from "../../components/Orden/CancelarOrden";
+import HeaderGeneral from "../../components/Header/HeaderGeneral";
 
 const Entrega: React.FC = () => {
   const [modal, setModal] = useState(false);
-  const [producto, setProducto] = useState('');
-  const [marca, setMarca] = useState('');
-  const [modelo, setModelo] = useState('');
-  const [cliente, setCliente] = useState('');
-  const [checkboxValues, setCheckboxValues] = useState<boolean[]>(Array(10).fill(false));
-  const [observaciones, setObservaciones] = useState('');
-  const [selectedOption, setSelectedOption] = useState(' ');
-  const [signature1, setSignature1] = useState('');
-  const [signature2, setSignature2] = useState('');
+  const [producto, setProducto] = useState("");
+  const [marca, setMarca] = useState("");
+  const [modelo, setModelo] = useState("");
+  const [cliente, setCliente] = useState("");
+  const [checkboxValues, setCheckboxValues] = useState<boolean[]>(
+    Array(10).fill(false)
+  );
+  const [observaciones, setObservaciones] = useState("");
+  const [selectedOption, setSelectedOption] = useState(" ");
+  const [signature1, setSignature1] = useState("");
+  const [signature2, setSignature2] = useState("");
   const textosCheckbox = [
-    'Enciende',
-    'Ingresa agua',
-    'No ingresa agua',
-    'Gira motor para los dos lados',
-    'Centrifuga',
-    'No centrifuga',
-    'Desagota',
-    'No desagota',
-    'Sin prueba de funcionamiento',
-    'No enciende',
+    "Enciende",
+    "Ingresa agua",
+    "No ingresa agua",
+    "Gira motor para los dos lados",
+    "Centrifuga",
+    "No centrifuga",
+    "Desagota",
+    "No desagota",
+    "Sin prueba de funcionamiento",
+    "No enciende",
   ];
 
   const handleConfirmarClick = () => {
@@ -59,7 +61,6 @@ const Entrega: React.FC = () => {
     console.log(dataToSend);
   };
 
- 
   const handleModal = () => {
     setModal(!modal);
   };
@@ -70,53 +71,79 @@ const Entrega: React.FC = () => {
         <IonHeader>
           <HeaderGeneral />
         </IonHeader>
-        <div className='diagnostico-ctn'>
-          <div className='section'>
+        <div className="diagnostico-ctn">
+          <div className="section">
             <h2>Entrega</h2>
-            <div className='item'>
+            <div className="item">
               <span>
                 <strong>Producto:</strong>
               </span>
               <IonInput
-                style={{ marginLeft: '10px' }}
+                style={{ marginLeft: "10px" }}
                 value={producto}
-                placeholder='Ingrese producto'
+                placeholder="Ingrese producto"
                 onIonChange={(e) => setProducto(e.detail.value!)}
               />
-              <IonIcon icon={pencilOutline} className='icon-pencil' style={{ fontSize: '22px' }} />
+              <IonIcon
+                icon={pencilOutline}
+                className="icon-pencil"
+                style={{ fontSize: "22px" }}
+              />
             </div>
-            <div className='item'>
+            <div className="item">
               <span>
                 <strong>Marca:</strong>
               </span>
-              <IonInput style={{ marginLeft: '10px' }} value={marca} placeholder='Ingrese marca' onIonChange={(e) => setMarca(e.detail.value!)} />
-              <IonIcon icon={pencilOutline} className='icon-pencil' style={{ fontSize: '22px' }} />
+              <IonInput
+                style={{ marginLeft: "10px" }}
+                value={marca}
+                placeholder="Ingrese marca"
+                onIonChange={(e) => setMarca(e.detail.value!)}
+              />
+              <IonIcon
+                icon={pencilOutline}
+                className="icon-pencil"
+                style={{ fontSize: "22px" }}
+              />
             </div>
-            <div className='item'>
+            <div className="item">
               <span>
                 <strong>Modelo:</strong>
               </span>
-              <IonInput style={{ marginLeft: '10px' }} value={modelo} placeholder='Ingrese modelo' onIonChange={(e) => setModelo(e.detail.value!)} />
-              <IonIcon icon={pencilOutline} className='icon-pencil' style={{ fontSize: '22px' }} />
+              <IonInput
+                style={{ marginLeft: "10px" }}
+                value={modelo}
+                placeholder="Ingrese modelo"
+                onIonChange={(e) => setModelo(e.detail.value!)}
+              />
+              <IonIcon
+                icon={pencilOutline}
+                className="icon-pencil"
+                style={{ fontSize: "22px" }}
+              />
             </div>
-            <div className='item'>
+            <div className="item">
               <span>
                 <strong>N° de cliente:</strong>
               </span>
               <IonInput
-                style={{ marginLeft: '10px' }}
+                style={{ marginLeft: "10px" }}
                 value={cliente}
-                placeholder='Ingrese N° de cliente'
+                placeholder="Ingrese N° de cliente"
                 onIonChange={(e) => setCliente(e.detail.value!)}
               />
-              <IonIcon icon={pencilOutline} className='icon-pencil' style={{ fontSize: '22px' }} />
+              <IonIcon
+                icon={pencilOutline}
+                className="icon-pencil"
+                style={{ fontSize: "22px" }}
+              />
             </div>
           </div>
-          <div className='section'>
+          <div className="section">
             <h2>Chequeo de funcionamiento</h2>
-            <div className='checkbox-container'>
+            <div className="checkbox-container">
               {textosCheckbox.map((texto, index) => (
-                <div key={index} className='checkbox-item'>
+                <div key={index} className="checkbox-item">
                   <IonCheckbox
                     checked={checkboxValues[index]}
                     onIonChange={(e) => {
@@ -124,66 +151,86 @@ const Entrega: React.FC = () => {
                       newCheckboxValues[index] = e.detail.checked;
                       setCheckboxValues(newCheckboxValues);
                     }}
-                    className='checkbox'
+                    className="checkbox"
                   />
                   <span>{texto}</span>
- 
                 </div>
               ))}
             </div>
           </div>
 
-          <div className='section'>
+          <div className="section">
             <h2>Tipo de entrega</h2>
-            <IonSelect value={selectedOption} placeholder='Seleccionar' onIonChange={(e) => setSelectedOption(e.detail.value)}>
-              <IonSelectOption value='option1'>Option 1</IonSelectOption>
-              <IonSelectOption value='option2'>Option 2</IonSelectOption>
-              <IonSelectOption value='option3'>Option 3</IonSelectOption>
+            <IonSelect
+              value={selectedOption}
+              placeholder="Seleccionar"
+              onIonChange={(e) => setSelectedOption(e.detail.value)}
+            >
+              <IonSelectOption value="option1">Option 1</IonSelectOption>
+              <IonSelectOption value="option2">Option 2</IonSelectOption>
+              <IonSelectOption value="option3">Option 3</IonSelectOption>
             </IonSelect>
           </div>
-          <div className='section'>
+          <div className="section">
             <h2>Observaciones</h2>
             <IonInput
-              className='obs-input'
+              className="obs-input"
               value={observaciones}
               onIonChange={(e) => setObservaciones(e.detail.value!)}
-              placeholder='Ingrese observaciones'
+              placeholder="Ingrese observaciones"
             />
           </div>
-          <div className='section'>
+          <div className="section">
             <h2>Conformidad de la entrega</h2>
             <span>Firma del cliente</span>
-            <SignatureCanvas penColor='black' canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }} />
+            <SignatureCanvas
+              penColor="black"
+              canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+            />
             <span>Firma del técnico</span>
-            <SignatureCanvas penColor='black' canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }} />
+            <SignatureCanvas
+              penColor="black"
+              canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+            />
           </div>
-          <div className='section'>
+          <div className="section">
             <h2>¿Nos recomendarias? </h2>
-            <IonRadioGroup value={selectedOption} onIonChange={(e) => setSelectedOption(e.detail.value)}>
+            <IonRadioGroup
+              value={selectedOption}
+              onIonChange={(e) => setSelectedOption(e.detail.value)}
+            >
               <IonRow>
                 <IonCol>
                   <IonItem>
                     <IonLabel>Si</IonLabel>
-                    <IonRadio slot='start' value='si' />
+                    <IonRadio slot="start" value="si" />
                   </IonItem>
                 </IonCol>
                 <IonCol>
                   <IonItem>
                     <IonLabel>No</IonLabel>
-                    <IonRadio slot='start' value='no' />
+                    <IonRadio slot="start" value="no" />
                   </IonItem>
                 </IonCol>
               </IonRow>
             </IonRadioGroup>
           </div>
-          <div className='section'>
-            <IonButton className='button' style={{ '--border-radius': '20px' }} onClick={handleConfirmarClick}>
+          <div className="section">
+            <IonButton
+              className="button"
+              style={{ "--border-radius": "20px" }}
+              onClick={handleConfirmarClick}
+            >
               Concretar entrega
             </IonButton>
-            <IonButton onClick={handleModal} className='button' style={{ '--border-radius': '20px' }}>
+            <IonButton
+              onClick={handleModal}
+              className="button"
+              style={{ "--border-radius": "20px" }}
+            >
               Cancelar orden
             </IonButton>
-            {modal && <CancelarOrden />}
+            {modal && <CancelarOrden onCancel={handleModal} />}
           </div>
         </div>
       </IonContent>
