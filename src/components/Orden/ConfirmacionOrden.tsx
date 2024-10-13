538e84b114768941c2c9a6689f77180f7abc62b7
facuzero <facuzero_95@hotmail.com>
2024-10-13 18:06:31 -0300

    enviar una notificacion desde orden

--- src/components/Orden/ConfirmacionOrden.tsx
+++ src/components/Orden/ConfirmacionOrden.tsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { IonButton, IonContent, IonHeader } from "@ionic/react";
-import { Geolocation } from "@capacitor/geolocation";
-import Map from "./Map";
-import HeaderGeneral from "../Header/HeaderGeneral";
-import "./ConfirmacionOrden.css";
-import { useLocation } from "react-router-dom";
-import { useOrden } from "../../pages/Orden/ordenContext";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { IonButton, IonContent, IonHeader } from '@ionic/react';
+import { Geolocation } from '@capacitor/geolocation';
+import Map from './Map';
+import HeaderGeneral from '../Header/HeaderGeneral';
+import './ConfirmacionOrden.css';
+import { useLocation } from 'react-router-dom';
+import { useOrden } from '../../pages/Orden/ordenContext';
+import socket from '../services/socketService';
+
 function ConfirmacionOrdenComponent() {
   const [position, setPosition] = useState({
     latitude: -33.9913,
@@ -17,34 +19,28 @@ function ConfirmacionOrdenComponent() {
   const history = useHistory();
   const location = useLocation();
   const { orden } = location.state as { orden: any };
-
   const obtenerPresupuesto = async (id_orden: any) => {
     try {
-      const response = await fetch(
-        `https://lv-back.online/presupuestos?ordenId=${id_orden}`
-      );
+      const response = await fetch(`https://lv-back.online/presupuestos?ordenId=${id_orden}`);
       const presupuestos = await response.json();
-      const presupuesto = presupuestos.find(
-        (presupuesto: any) => presupuesto.id_orden === id_orden
-      );
+      const presupuesto = presupuestos.find((presupuesto: any) => presupuesto.id_orden === id_orden);
       if (presupuesto) {
-        console.log(
-          `Se encontró un presupuesto asociado al id_orden ${id_orden}`
-        );
+        console.log(`Se encontró un presupuesto asociado al id_orden ${id_orden}`);
         return presupuesto;
       } else {
-        console.log(
-          `No se encontró ningún presupuesto con el id_orden ${id_orden}`
-        );
+        console.log(`No se encontró ningún presupuesto con el id_orden ${id_orden}`);
         return null;
       }
     } catch (error) {
-      console.error("Error, presupuesto no encontrado.", error);
+      console.error('Error, presupuesto no encontrado.', error);
       return null;
     }
   };
 
   useEffect(() => {
+    socket.on('todasNotificaciones', (data) => {
+      console.log('Notificación recibida en Confirmación de Orden:', data);
+    });
     if (orden && orden.Cliente) {
       setPosition({
         latitude: orden.Cliente.latitud,
@@ -63,6 +59,11 @@ function ConfirmacionOrdenComponent() {
     };
 
     fetchData();
+
+    // Limpieza del evento cuando se desmonta el componente
+    return () => {
+      socket.off('todasNotificaciones');
+    };
   }, [orden]);
 
   const handleButtonClick = (path: any, orden = null) => {
@@ -76,21 +77,24 @@ function ConfirmacionOrdenComponent() {
     }
   };
 
+  const handleSocket = () => {
+    socket.emit('todasNotificaciones', { mensaje: 'Mensaje desde Confirmación de Orden' });
+  };
   return (
-    <IonContent className="confirmacion-orden-container">
+    <IonContent className='confirmacion-orden-container'>
       <IonHeader>
         <HeaderGeneral />
       </IonHeader>
 
       {orden && (
         <div>
-          <div className="confirmacion-orden-top-box">
+          <div className='confirmacion-orden-top-box'>
             <h1>
               <strong>Orden activa</strong>
             </h1>
             <h3>#{orden.id}</h3>
           </div>
-          <div className="confirmacion-orden-medium-box">
+          <div className='confirmacion-orden-medium-box'>
             <h3>
               <strong>Datos del cliente</strong>
             </h3>
@@ -109,46 +113,27 @@ function ConfirmacionOrdenComponent() {
               </h4>
             </div>
             <div>{!loading && <Map position={position} zoom={13} />}</div>
-            {orden &&
-            orden.Presupuesto &&
-            orden.id_tipo_estado === 1 &&
-            (orden.Presupuesto.id_estado_presupuesto === 5 ||
-              orden.Presupuesto.id_estado_presupuesto === 4) ? (
+            {orden && orden.Presupuesto && orden.id_tipo_estado === 1 && (orden.Presupuesto.id_estado_presupuesto === 5 || orden.Presupuesto.id_estado_presupuesto === 4) ? (
               <>
-                <IonButton onClick={() => handleButtonClick("/entrega", orden)}>
-                  Entrega
-                </IonButton>
-                <IonButton
-                  onClick={() => handleButtonClick("/facturacion", orden)}
-                >
-                  Facturación
-                </IonButton>
+                <IonButton onClick={() => handleButtonClick('/entrega', orden)}>Entrega</IonButton>
+                <IonButton onClick={() => handleButtonClick('/facturacion', orden)}>Facturación</IonButton>
               </>
             ) : (
               <>
-                <IonButton
-                  onClick={() => handleButtonClick("/diagnostico", orden)}
-                >
-                  Diagnóstico
-                </IonButton>
-                <IonButton
-                  onClick={() => handleButtonClick("/presupuesto", orden)}
-                >
-                  Presupuestar
-                </IonButton>
-                <IonButton onClick={() => handleButtonClick("/chat", orden)}>
-                  Chat
-                </IonButton>
+                <IonButton onClick={() => handleButtonClick('/diagnostico', orden)}>Diagnóstico</IonButton>
+                <IonButton onClick={() => handleButtonClick('/presupuesto', orden)}>Presupuestar</IonButton>
+                <IonButton onClick={() => handleButtonClick('/chat', orden)}>Chat</IonButton>
               </>
             )}
           </div>
         </div>
       )}
 
-      <div className="confirmacion-orden-bottom-box">
+      <div className='confirmacion-orden-bottom-box'>
         <h4>Cotización</h4>
         <h3>${total}</h3>
         <button>Cerrar orden</button>
+        <button onClick={handleSocket}>Enviar algo al Sokcet</button>
       </div>
     </IonContent>
   );
