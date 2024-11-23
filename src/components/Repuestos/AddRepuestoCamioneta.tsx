f30ad6d2430b35016d3008e85f3224c97e4557f9
fakumetal <faku.metal@gmail.com>
2024-11-23 08:53:38 -0300

    fotos guardadas en /uploads

--- src/components/Repuestos/AddRepuestoCamioneta.tsx
+++ src/components/Repuestos/AddRepuestoCamioneta.tsx
@@ -5,6 +5,7 @@ import { guardarStockCamioneta, modificarStockCamioneta } from './FetchsRepuesto
 import { useOrden } from '../../Provider/Provider';
 import GlobalRefresher from '../Refresher/GlobalRefresher';
 import NotaCompra from './nota de compra/notaCompra';
+import FotoComprobante from './FotoComprobante'; 
 import RepuestoUsado from './repuesto usado/RepuestoUsado';
 const AddRepuestoCamioneta: React.FC = () => {
   const [nombreRepuesto, setNombreRepuesto] = useState<string>('');
@@ -15,9 +16,14 @@ const AddRepuestoCamioneta: React.FC = () => {
   const [modalNota, setModalNota] = useState(false);
   const [modalRep, setModalRep] = useState(false);
   const [checkbox, setCheckbox] = useState(false);
-
+  const [precio, setPrecio] = useState("");
   const { repuestosCamioneta } = useOrden();
   const [localRepuestosCamioneta, setLocalRepuestosCamioneta] = useState<any[]>(repuestosCamioneta);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isFotoModalOpen, setIsFotoModalOpen] = useState(false);  // Nuevo estado para el modal de foto
+  const [comprobanteFoto, setComprobanteFoto] = useState<string | undefined>(undefined);  // URL de la foto del comprobante
+
+
 
   useEffect(() => {
     const empleadoId = localStorage.getItem('empleadoId');
@@ -33,39 +39,160 @@ const AddRepuestoCamioneta: React.FC = () => {
     setLocalRepuestosCamioneta(repuestosCamioneta);
   }, [repuestosCamioneta]);
 
-  const handleAgregarRepuesto = async () => {
-    if (!nombreRepuesto.trim()) {
-      setShowAlert({ success: false, message: 'El nombre del repuesto no puede estar vacío.' });
+
+  ///////// AGREGAR REPUESTO 
+
+  const handleAgregarRepuesto = () => {
+    if (!nombreRepuesto.trim() || cantidad <= 0 || !precio || parseFloat(precio) <= 0) {
+      setShowAlert({ success: false, message: 'Todos los campos deben ser válidos antes de continuar.' });
       return;
     }
-    if (confirm('Desea agregar el reppuesto?')) {
-      const repuesto = {
-        nombre: nombreRepuesto,
-        cantidad: cantidad,
-        id_empleado: idEmpleado,
-        id_repuesto: null,
-        lote: '',
-        usado: checkbox,
-      };
-      console.log(repuesto);
-    }
+    setIsModalOpen(true); // Abrir modal
+  };
 
-    /* 
-    //@ts-ignore
-    
+  // const handleAgregarRepuesto = async () => {
+  //   if (!nombreRepuesto.trim()) {
+  //     setShowAlert({ success: false, message: 'El nombre del repuesto no puede estar vacío.' });
+  //     return;
+  //   }
+  
+  //   if (cantidad <= 0) {
+  //     setShowAlert({ success: false, message: 'La cantidad debe ser mayor a 0.' });
+  //     return;
+  //   }
+  
+  //   if (!precio || parseFloat(precio) <= 0) {
+  //     setShowAlert({ success: false, message: 'El precio debe ser mayor a 0.' });
+  //     return;
+  //   }
+  
+  //   if (confirm('¿Desea agregar el repuesto?')) {
+  //     const repuesto = {
+  //       nombre: nombreRepuesto,
+  //       cantidad: cantidad,
+  //       id_empleado: idEmpleado,
+  //       id_repuesto: null,
+  //       lote: '',
+  //       isUsado: checkbox,
+  //       precio: parseFloat(precio), 
+  //     };
+  
+  //     console.log(repuesto);
+  
+  //     //@ts-ignore
+  //     const success = await guardarStockCamioneta(repuesto);
+  
+  //     if (success) {
+  //       setShowAlert({ success: true, message: 'Repuesto agregado exitosamente al stock de la camioneta.' });
+  //       setNombreRepuesto('');
+  //       setCantidad(0);
+  //       setPrecio('');
+  //       setCheckbox(false);
+  
+       
+  //       setLocalRepuestosCamioneta((prev) => [...prev, { ...repuesto, id: new Date().getTime() }]);  
+  //     } else {
+  //       setShowAlert({ success: false, message: 'Error al agregar el repuesto al stock de la camioneta.' });
+  //     }
+  //   }
+  // };
+
+
+
+  const handleCargarComprobante = () => {
+    setIsFotoModalOpen(true);
+  };
+
+  // Guarda la foto del comprobante y cierra el modal
+  const handlePhotoTaken = (photoUrl: string) => {
+    setComprobanteFoto(photoUrl);
+    setIsFotoModalOpen(false);
+  };
+
+
+
+
+
+
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    // Nombre de la foto (puedes obtenerlo dinámicamente de alguna base de datos o del estado)
+    const filename = '1732303988304-comprobante.jpg';
+
+    // Construir la URL de la imagen
+    const url = `https://lv-back.online/uploads/${filename}`;
+
+    // Hacer el fetch para obtener la imagen
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          setImageUrl(url);  // Si la respuesta es exitosa, mostrar la imagen
+        } else {
+          console.error('Error al obtener la imagen:', response.status);
+        }
+      })
+      .catch(error => {
+        console.error('Error al hacer el fetch:', error);
+      });
+  }, []);
+
+  console.log("IMAGE YRL", imageUrl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Enviar los datos a la base de datos, incluyendo la foto del comprobante
+  const handleGuardarConComprobante = async () => {
+    const repuesto = {
+      nombre: nombreRepuesto,
+      cantidad,
+      precio: parseFloat(precio),
+      id_empleado: idEmpleado,
+      comprobante_compra: comprobanteFoto,  // Aquí pasamos la URL de la foto
+      isUsado: checkbox,
+      lote: '',
+    };
+
     const success = await guardarStockCamioneta(repuesto);
-
     if (success) {
-      setShowAlert({ success: true, message: 'Repuesto agregado exitosamente al stock de la camioneta.' });
+      setShowAlert({ success: true, message: 'Repuesto agregado exitosamente al stock con comprobante.' });
       setNombreRepuesto('');
       setCantidad(0);
-
-      // Actualizar localRepuestosCamioneta agregando el nuevo repuesto
-      setLocalRepuestosCamioneta((prev) => [...prev, { ...repuesto, id: new Date().getTime() }]); // Usa un id temporal
+      setPrecio('');
+      setComprobanteFoto(undefined);  // Limpiar la foto después de guardar
     } else {
-      setShowAlert({ success: false, message: 'Error al agregar el repuesto al stock de la camioneta.' });
-    } */
+      setShowAlert({ success: false, message: 'Error al guardar el repuesto con comprobante.' });
+    }
+    setIsModalOpen(false);  // Cerrar el modal principal
   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   const handleCantidadChange = (id: number, nuevaCantidad: string) => {
     const cantidadNumerica = parseInt(nuevaCantidad, 10);
     if (!isNaN(cantidadNumerica)) {
@@ -116,6 +243,31 @@ const AddRepuestoCamioneta: React.FC = () => {
   const handleModalRep = () => {
     setModalRep(!modalRep);
   };
+
+  const handleGuardarSinComprobante = async () => {
+    const repuesto = {
+      nombre: nombreRepuesto,
+      cantidad,
+      precio: parseFloat(precio),
+      id_empleado: idEmpleado,  
+      id_repuesto: null,
+      isUsado: checkbox,
+      lote: '',
+    };
+
+    const success = await guardarStockCamioneta(repuesto);
+    if (success) {
+      setShowAlert({ success: true, message: 'Repuesto agregado exitosamente al stock.' });
+      setNombreRepuesto('');
+      setCantidad(0);
+      setPrecio('');
+      setCheckbox(false);
+    } else {
+      setShowAlert({ success: false, message: 'Error al guardar el repuesto.' });
+    }
+    setIsModalOpen(false); 
+  };
+
   return (
     <GlobalRefresher>
       <IonPage>
@@ -132,16 +284,23 @@ const AddRepuestoCamioneta: React.FC = () => {
               <IonInput value={cantidad} onIonChange={(e) => setCantidad(parseInt(e.detail.value!, 10) || 0)} style={{ width: '80px', textAlign: 'right', margin: '0 8px' }} />
               <IonButton onClick={() => setCantidad((prev) => prev + 1)}>+</IonButton>
               <IonButton onClick={() => setCantidad((prev) => (prev > 0 ? prev - 1 : 0))}>–</IonButton>
-              <IonCheckbox justify='end' style={{ width: '30%' }} onClick={() => setCheckbox(!checkbox)}>
+              <IonCheckbox style={{ width: '30%' }} onClick={() => setCheckbox(!checkbox)}>
                 usado?
               </IonCheckbox>
+              <IonInput
+  value={precio}
+  type="number"
+  onIonChange={(e) => setPrecio(e.detail.value || '')}
+  placeholder="$0.00"
+  style={{ width: '100px', textAlign: 'right', marginLeft: '8px' }}
+/>
             </div>
           </IonItem>
 
           <IonButton expand='block' onClick={handleAgregarRepuesto}>
             Agregar Repuesto
           </IonButton>
-
+ 
           <IonButton expand='block' onClick={handleModalNota}>
             Nota de compra
           </IonButton>
@@ -188,7 +347,32 @@ const AddRepuestoCamioneta: React.FC = () => {
               </IonItem>
             )}
           </IonList>
+   {/* Modal */}
+          <IonModal isOpen={isModalOpen} onDidDismiss={() => setIsModalOpen(false)}>
+            <IonContent className="ion-padding">
+              <h2>Cargar Comprobante</h2>
+              <p>¿Quieres cargar un comprobante de compra o continuar sin él?</p>
+              <IonButton expand="block" onClick={handleCargarComprobante}>
+                Cargar Comprobante
+              </IonButton>
+              <IonButton expand="block" color="secondary" onClick={handleGuardarConComprobante}>
+                Continuar con comprobante
+              </IonButton>
 
+              <IonButton expand="block" color="secondary" onClick={handleGuardarSinComprobante}>
+                Continuar sin comprobante
+              </IonButton>
+
+              <IonButton expand="block" fill="outline" onClick={() => setIsModalOpen(false)}>
+                Cancelar
+              </IonButton>
+            </IonContent>
+          </IonModal>
+          {isFotoModalOpen && (
+            <IonModal isOpen={isFotoModalOpen} onDidDismiss={() => setIsFotoModalOpen(false)}>
+              <FotoComprobante onPhotoTaken={handlePhotoTaken}   />
+            </IonModal>
+          )}
           {showAlert && <IonAlert isOpen={!!showAlert} onDidDismiss={() => setShowAlert(null)} header={showAlert.success ? 'Éxito' : 'Error'} message={showAlert.message} buttons={['OK']} />}
         </IonContent>
       </IonPage>
